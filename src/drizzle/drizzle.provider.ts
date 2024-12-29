87250d8081009a0cc48f3014b4b560f411922fc2
imadusanka <isurumadusanka398@gmail.com>
2024-12-29 11:40:26 +0530

    feat: get port form env

--- src/drizzle/drizzle.provider.ts
+++ src/drizzle/drizzle.provider.ts
@@ -10,7 +10,7 @@ export const drizzleProvider = [
     useFactory: async () => {
       const pool = new Pool({
         host: process.env.DB_HOST,
-        port: 5432,
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -8,7 +8,7 @@ import { departments } from './data';
 const main = async () => {
   const pool = new Pool({
     host: process.env.DB_HOST,
-    port: 5432,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
